0fef0ce3b01449d6e1c26e4ff5527d69b58d5e02
David Taylor <david.taylor@informed.com>
2017-12-14 14:20:40 +0000

    incident recording question flow changes

--- Incident-Recording-Prototype/app/routes.js
+++ Incident-Recording-Prototype/app/routes.js
@@ -30,4 +30,118 @@ router.get('/level-of-harm', function (req, res) {
     }
 });
 
+router.post('/incident-subcategory', function (req, res) {
+    var incidentCategory = req.body["incident-type"];
+    req.session.incidentCategory = incidentCategory;
+
+    if (incidentCategory == 'Access, admission, transfer, discharge (including missing patient)') {
+        res.redirect('/incident-subcategory/access');
+    }
+    else if (incidentCategory == "Clinical assessment (including diagnosis, scans, tests, assessments)") {
+        res.redirect('/incident-subcategory/clinical-assessment');
+    }
+    else if (incidentCategory == "Consent, communication, confidentiality)") {
+        res.redirect('/incident-subcategory/consent');
+    }
+    else if (incidentCategory == "Disruptive, aggressive behaviour (includes patient to patient))") {
+        res.redirect('/incident-subcategory/disruptive');
+    }
+    else if (incidentCategory == "Documentation (including electronic & paper records, identification and charts)") {
+        res.redirect('/incident-subcategory/documentation');
+    }
+    else if (incidentCategory == "Implementation of care and ongoing monitoring / review") {
+        res.redirect('/incident-subcategory/implementation');
+    }
+    else if (incidentCategory == "Infection Control Incident") {
+        res.redirect('/incident-subcategory/infection');
+    }
+    else if (incidentCategory == "Infrastructure (including staffing, facilities, environment)") {
+        res.redirect('/incident-subcategory/infrastructure');
+    }
+    else if (incidentCategory == "Medical device / equipment") {
+        res.redirect('/incident-subcategory/device');
+    }
+    else if (incidentCategory == "Medication") {
+        res.redirect('/incident-subcategory/medication');
+    }
+    else if (incidentCategory == "Patient abuse (by staff / third party)") {
+        res.redirect('/incident-subcategory/abuse');
+    }
+    else if (incidentCategory == "Patient accident") {
+        res.redirect('/incident-subcategory/accident');
+    }
+    else if (incidentCategory == "Self-harming behaviour") {
+        res.redirect('/incident-subcategory/self-harming');
+    }
+    else if (incidentCategory == "Treatment, procedure") {
+        res.redirect('/incident-subcategory/treatment');
+    }
+    else if (incidentCategory == "Maternal fetal neonatal incidents CNST triggers") {
+        res.redirect('/incident-subcategory/maternal');
+    }
+    else if (incidentCategory == "Pressure ulcer") {
+        res.redirect('/incident-subcategory/ulcer');
+    }
+    else if (incidentCategory == "Other") {
+        res.redirect('/incident-subcategory/other');
+    }
+});
+
+router.get('/incident-subcategory', function (req, res) {
+    var incidentCategory = req.session.incidentCategory;
+
+    if (incidentCategory == 'Access, admission, transfer, discharge (including missing patient)') {
+        res.redirect('/incident-subcategory/access');
+    }
+    else if (incidentCategory == "Clinical assessment (including diagnosis, scans, tests, assessments)") {
+        res.redirect('/incident-subcategory/clinical-assessment');
+    }
+    else if (incidentCategory == "Consent, communication, confidentiality)") {
+        res.redirect('/incident-subcategory/consent');
+    }
+    else if (incidentCategory == "Disruptive, aggressive behaviour (includes patient to patient))") {
+        res.redirect('/incident-subcategory/disruptive');
+    }
+    else if (incidentCategory == "Documentation (including electronic & paper records, identification and charts)") {
+        res.redirect('/incident-subcategory/documentation');
+    }
+    else if (incidentCategory == "Implementation of care and ongoing monitoring / review") {
+        res.redirect('/incident-subcategory/implementation');
+    }
+    else if (incidentCategory == "Infection Control Incident") {
+        res.redirect('/incident-subcategory/infection');
+    }
+    else if (incidentCategory == "Infrastructure (including staffing, facilities, environment)") {
+        res.redirect('/incident-subcategory/infrastructure');
+    }
+    else if (incidentCategory == "Medical device / equipment") {
+        res.redirect('/incident-subcategory/device');
+    }
+    else if (incidentCategory == "Medication") {
+        res.redirect('/incident-subcategory/medication');
+    }
+    else if (incidentCategory == "Patient abuse (by staff / third party)") {
+        res.redirect('/incident-subcategory/abuse');
+    }
+    else if (incidentCategory == "Patient accident") {
+        res.redirect('/incident-subcategory/accident');
+    }
+    else if (incidentCategory == "Self-harming behaviour") {
+        res.redirect('/incident-subcategory/self-harming');
+    }
+    else if (incidentCategory == "Treatment, procedure") {
+        res.redirect('/incident-subcategory/treatment');
+    }
+    else if (incidentCategory == "Maternal fetal neonatal incidents CNST triggers") {
+        res.redirect('/incident-subcategory/maternal');
+    }
+    else if (incidentCategory == "Pressure ulcer") {
+        res.redirect('/incident-subcategory/ulcer');
+    }
+    else if (incidentCategory == "Other") {
+        res.redirect('/incident-subcategory/other');
+    }
+});
+
+
 module.exports = router;
