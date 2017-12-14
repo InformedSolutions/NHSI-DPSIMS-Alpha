2ce55f765c88786409d9c4ff542e78a64751ec87
David Taylor <david.taylor@informed.com>
2017-12-14 10:51:04 +0000

    refactored code for MDS prototype

--- Incident-Recording-Prototype/app/routes.js
+++ Incident-Recording-Prototype/app/routes.js
@@ -6,58 +6,28 @@ router.get('/', function (req, res) {
   res.render('index')
 });
 
-router.post('/mds1', function (req, res) {
-    res.render('record-type/index');
-});
-
-router.get('/mds1', function (req, res) {
-    res.render('record-type/index');
-});
-
-router.post('/mds2', function (req, res) {
+router.post('/selected-type', function (req, res) {
     var recordType = req.body["record-type"];
     req.session.recordType = recordType;
 
     if (recordType == 'Risk') {
-        res.render('risk/index');
+        res.redirect('/risk-details');
     }
     else if (recordType == "PSI") {
-        res.render('level-of-harm/incident');
+        res.redirect('/level-of-harm/incident');
     } else {
-        res.render('level-of-harm/outcome');
+        res.redirect('/level-of-harm/outcome');
     }
 });
 
-router.get('/mds2', function (req, res) {
+router.get('/level-of-harm', function (req, res) {
     var recordType = req.session.recordType;
 
-    if (recordType == 'Risk') {
-        res.render('risk/index');
-    }
-    else if (recordType == "PSI") {
-        res.render('level-of-harm/incident');
+    if (recordType == "PSI") {
+        res.redirect('/level-of-harm/incident');
     } else {
-        res.render('level-of-harm/outcome');
+        res.redirect('/level-of-harm/outcome');
     }
-});
-
-router.post('/mds3', function (req, res) {
-    res.render('details/index');
-});
-
-router.get('/mds3', function (req, res) {
-    var recordType = req.session.recordType;
-
-    if (recordType == 'Risk') {
-        res.render('risk/index');
-    }
-     else {
-        res.render('details/index');
-    }
-});
-
-router.post('/check-your-answers', function (req, res) {
-    res.render('check-your-answers/index');
 });
 
 module.exports = router;
