fcb9c7d8c678e8e07d186aa885bbb8fb970c1430
David Taylor <david.taylor@informed.com>
2018-01-04 16:29:47 +0000

    test azure env vars

--- Incident-Recording-Prototype/start.js
+++ Incident-Recording-Prototype/start.js
@@ -22,7 +22,7 @@ if (!envExists) {
 
 var spawn = require('cross-spawn')
 
-var env = process.env.PATH + "D:\\Program Files (x86)\\gulp\\3.9.0.1";
+var env = process.env.PATH + "D:\\Program Files (x86)\\SiteExtensions\\Kudu\\69.61204.3166\\bin\\NativeBinaries/x86;D:\\home\\site\\deployments\\tools;D:\\Program Files (x86)\\SiteExtensions\\Kudu\\69.61204.3166\\bin\\Scripts;D:\\Program Files (x86)\\MSBuild\\14.0\\Bin;D:\\Program Files (x86)\\Git\\cmd;D:\\Program Files (x86)\\Microsoft Visual Studio 11.0\\Common7\\IDE\\CommonExtensions\\Microsoft\\TestWindow;D:\\Program Files (x86)\\Microsoft SQL Server\\110\\Tools\\Binn;D:\\Program Files (x86)\\Microsoft SDKs\\F#\\3.1\\Framework\\v4.0;D:\\Program Files (x86)\\Git\\bin;D:\\Program Files (x86)\\Git\\usr\\bin;D:\\Program Files (x86)\\Git\\mingw32\\bin;D:\\Program Files (x86)\\nodejs\\0.10.28;D:\\Program Files (x86)\\npm\\1.4.28;C:\\DWASFiles\\Sites\\#1psims-incident-recording\\AppData\\npm;D:\\Program Files (x86)\\bower\\1.7.9;D:\\Program Files (x86)\\grunt\\0.1.13;D:\\Program Files (x86)\\gulp\\3.9.0.1;D:\\Program Files (x86)\\funcpack\\0.2.2;D:\\Program Files (x86)\\nodejs;D:\\Windows\\system32;D:\\Windows;D:\\Windows\\System32\\Wbem;D:\\Windows\\System32\\WindowsPowerShell\\v1.0\\;D:\\Users\\Administrator\\AppData\\Local\\Microsoft\\WindowsApps;;D:\\Program Files (x86)\\dotnet;D:\\Windows\\system32\\config\\systemprofile\\AppData\\Local\\Microsoft\\WindowsApps;D:\\Program Files (x86)\\Git\\cmd;D:\\Program Files (x86)\\PHP\\v5.6;D:\\Python27;";
 console.log(env);
 
 process.env['FORCE_COLOR'] = 1
