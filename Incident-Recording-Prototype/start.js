ab1c62c0f493ecacd71d5130802783cc9fc40f36
David Taylor <david.taylor@informed.com>
2018-01-05 13:24:52 +0000

    update env parameter

--- Incident-Recording-Prototype/start.js
+++ Incident-Recording-Prototype/start.js
@@ -23,7 +23,7 @@ if (!envExists) {
 var spawn = require('cross-spawn')
 
 process.env['FORCE_COLOR'] = 1
-var gulp = spawn('gulp', {env: 'node_modules/.bin'})
+var gulp = spawn('gulp', {env: {PATH: process.env.PATH + 'node_modules/.bin'}})
 gulp.stdout.pipe(process.stdout)
 gulp.stderr.pipe(process.stderr)
 process.stdin.pipe(gulp.stdin)
