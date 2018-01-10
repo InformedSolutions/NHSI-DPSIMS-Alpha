38c42ab8638d736f9ff6bcbf83d7662bc2c0982f
David Taylor <david.taylor@informed.com>
2018-01-10 14:08:15 +0000

    renamed category to theme

--- Incident-Recording-Prototype/app/taxonomy.js
+++ Incident-Recording-Prototype/app/taxonomy.js
@@ -46,7 +46,7 @@ var taxonomy =
                 "name": "Not Known"
             }
         ],
-        'categories': [
+        'themes': [
             {
                 'id': "accident",
                 'name': "Patient Accident"
@@ -106,6 +106,122 @@ var taxonomy =
             {
                 'id': "other",
                 'name': "Other"
+            }
+        ],
+        'categories' : [
+            {
+                'id': "A",
+                'name': "Access, admission, transfer, discharge (including missing patient)",
+                'type': [
+                    "incident",
+                    "outcome"
+                ],
+                'subCategories' : [
+                    {
+                        'id': "A0100",
+                        'name': "Absconder / missing patient",
+                        'type': "outcome"
+                    },
+                    {
+                        'id': "A0200",
+                        'name': "Access / admission - delay / failure in access to hospital / care",
+                        'type': "incident"
+                    },
+                    {
+                        'id': "A0800",
+                        'name': "Discharge - inappropriate",
+                        'type': "incident"
+                    }
+                ]
+            },
+            {
+                'id': "B",
+                'name': "Clinical assessment (including diagnosis, scans, tests, assessments)",
+                'type': [
+                    "incident",
+                    "outcome"
+                ],
+                'subCategories' : [
+                    {
+                        'id': "B1200",
+                        'name': "Scans / X-rays / specimens - mislabelled / unlabelled",
+                        'type': "incident"
+                    },
+                    {
+                        'id': "B1700",
+                        'name': "Test results / reports - missing",
+                        'type': "outcome"
+                    },
+                    {
+                        'id': "B1800",
+                        'name': "Tests - failure / delay to undertake",
+                        'type': "incident"
+                    }
+                ]
+            },
+            {
+                'id': "D",
+                'name': "Disruptive, aggressive behaviour (includes patient to patient)",
+                'type': [
+                    "incident"
+                ],
+                'subCategories' : [
+                    {
+                        'id': "D0100",
+                        'name': "Physical",
+                        'type': "incident"
+                    },
+                    {
+                        'id': "D0200",
+                        'name': "Racial",
+                        'type': "incident"
+                    },
+                    {
+                        'id': "D0400",
+                        'name': "Verbal",
+                        'type': "incident"
+                    }
+                ]
+            },
+            {
+                'id': "F",
+                'name': "Infection Control Incident",
+                'type': [
+                    "incident",
+                    "outcome"
+                ],
+                'subCategories' : [
+                    {
+                        'id': "F0300",
+                        'name': "Failure of sterilisation or contamination of equipment",
+                        'type': "incident"
+                    },
+                    {
+                        'id': "F0500",
+                        'name': "Infection - wound",
+                        'type': "outcome"
+                    },
+                    {
+                        'id': "F1000",
+                        'name': "Treatment / procedure - delay / failure",
+                        'type': "incident"
+                    }
+                ]
+            },
+            {
+                'id': "Q",
+                'name': "Pressure Ulcer",
+                'type': [
+                    "outcome"
+                ],
+                'subCategories' : [
+                    {
+                        'id': "Q0100",
+                        'name': "Acquired during NHS care",
+                        'type': "outcome",
+                        'linkedCategories': ["F0300"]
+                    }
+                ]
             }
         ]
     };
